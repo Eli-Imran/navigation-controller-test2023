9e346a9d2c57cc8b93c88c6f854a3e97000fe3d6
Imran Ali <eli@ui-db.com>
2023-05-20 22:24:42 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import Navigator from 'react.cordova-navigation_controller';
+import Step1 from './Step1';
+import Step2 from './Step2';
+import Step3 from './Step3';
 import './App.css';
+const App = (props) => {
 
-function App() {
+  const [nowPage, setNowPage] = useState("");
+  const navigatorRef = useRef(null);
+  const menuClick = (e, goToPage) => {
+    if (navigatorRef.current) {
+      navigatorRef.current.changePage(goToPage);
+      document.getElementsByClassName("active")[0].className = "";
+      e.currentTarget.className = "active";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navigator
+      onRef={ref => (navigatorRef.current = ref)}
+      key="Navigator"
+      myApp={this}
+      onChangePage={page => {
+        setNowPage(page);
+      }}
+    >
+      <Step1 key="step1" levelPage={1} navigatorRef={navigatorRef.current} />
+      <Step2 key="step2" levelPage={2} navigatorRef={navigatorRef.current} />
+      <Step3 key="step3" levelPage={3} navigatorRef={navigatorRef.current} />
+    </Navigator>
   );
-}
 
+}
 export default App;
---
+++ src/Step3.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Step3(props) {
+    return (
+        <div className='step3'>
+            Step3
+            <button onClick={() => { props.navigatorRef.changePage("step2"); }} >
+                Back
+            </button>
+        </div>
+    )
+}
+export default Step3
